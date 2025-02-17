7e0b731c8e199599b91d4618cda24264d18d9a2e
webuiexpert <webuiexpert929@gmail.com>
2025-02-17 19:46:24 +0530

    add three pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightAaqua: '#1AFFE4', // bright aqua
+        teal: '#14B8A6', // teal
+        darkTeal: '#0A574E', // dark teal
+        lightGray: '#E6E6E6', // light gray
+        darkGray: '#292929', // dark gray
+      },
+    },
   },
   plugins: [],
 }
